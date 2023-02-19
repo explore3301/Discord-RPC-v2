808f12d832dd6063792025df24f4bb7e8037ccda
PAIN <97961226+Pain6900@users.noreply.github.com>
2023-02-19 20:30:37 +0530

    Update xxx.js

--- xxx.js
+++ xxx.js
@@ -1,7 +1,15 @@
-const xxx = process.env.TOKEN?
-  [process.env.TOKEN]:
-  ["token 1","token 2"]
+const data = process.env.TOKEN?
+//for single token enter TOKEN in secrets / .env
+  [
+    process.env.TOKEN
+  ]:
+//for multiple tokens enter tokens below 
+  [
+    "token 1",
+    "token 2",
+    "token 3"
+  ]
 
 module.exports = {
-  xxx
+  data
 }
