209df9d4d9434385d1b7cb3aaf212b93b6d55263
PAIN <97961226+Pain6900@users.noreply.github.com>
2023-02-19 19:23:14 +0530

    Update exe.js

--- exe.js
+++ exe.js
@@ -2,8 +2,7 @@ const { rpc } = require("./rpc.js")
 const { xxx } = require("./xxx.js")
 const Discord = require("discord.js-selfbot-v13")
 
-async function task() {
-  for (i = 0; i < xxx.length; i++) {
+for (i = 0; i < xxx.length; i++) {
 
     let client = new Discord.Client({ checkUpdate: false })
     let value = xxx[i]
@@ -19,10 +18,3 @@ async function task() {
     })
 
   }
-}
-
-let exe = task()
-
-module.exports = {
-  exe
-}
