206f9f4525ee3cc7199b32e72ffb848b79768f33
PAIN <97961226+Pain6900@users.noreply.github.com>
2023-02-19 23:10:08 +0530

    Update exec.js

--- exec.js
+++ exec.js
@@ -3,13 +3,18 @@ const { data } = require("./data.js")
 const Discord = require("discord.js-selfbot-v13")
 
 
-data.forEach((value)=>{
+data.forEach(async(value) => {
   let client = new Discord.Client({ checkUpdate: false })
-  
-    client.login(value).catch((err) => {
-      console.log("❌ : " + value + " : " + err.code)})
 
-    client.on("ready", async () => {
-      client.user.setActivity(rpcx)
-      console.log("✅ : " + value + " : " + client.user.username)})
+  client.login(value).catch((err) => {
+    console.log("❌ : " + value + " : " + err.code)
+  })
+
+  client.on("ready", async () => {
+    client.user.setActivity(rpcx)
+    console.log("✅ : " + value + " : " + client.user.username)
+  })
+
+  setInterval(()=>{if(!client || !client.user)process.kill(1)},15000)
+  
 })
