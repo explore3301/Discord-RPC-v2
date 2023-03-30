71ea63142925e9a76d2842c42f1500ef646b6cf5
PAIN <97961226+Pain6900@users.noreply.github.com>
2023-03-30 11:36:14 +0530

    Update exec.js

--- exec.js
+++ exec.js
@@ -15,10 +15,14 @@ const Discord = require("discord.js-selfbot-v13")
 
   setInterval(async()=>{
     if(!client.isReady())process.kill(1)
-    await client.checkUpdate();
-    client.on('update', async () => {
-    const { exec } = require('child_process');
-    await exec('npm i discord.js-selfbot-v13@latest')
-    await process.kill(1)
-    })
+    await client.checkUpdate()
   },15000)
+
+client.on('update', async (x, y) => {
+  if(x !== y)
+  {
+    const { exec } = require('child_process')
+    exec('npm i discord.js-selfbot-v13@latest')
+    await process.kill(1)
+  }
+})
